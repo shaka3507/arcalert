4f897938d2c05223e8d085b868ec0e3751fa0316
shakalee14 <shakalee14@gmail.com>
2025-03-03 17:46:34 -0600

    wip

---
+++ app/alert/page.tsx
@@ -0,0 +1,27 @@
+import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
+import ConversationalForm from "@/components/ui/alert/conversational-form";
+import { createClient } from "@/utils/supabase/server";
+import { InfoIcon } from "lucide-react";
+import { redirect } from "next/navigation";
+
+import React, { useState } from 'react';
+
+
+export default async function ProtectedPage() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-12">
+        <h2>Create Alert </h2>
+        <ConversationalForm userId={user.id} />
+    </div>
+  );
+}
---
+++ components/arc-hero.tsx
@@ -0,0 +1,3 @@
+export default function ArcHero() {
+  return (<div><h1>Arc Alert</h1></div>)
+}
